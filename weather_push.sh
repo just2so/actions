adea0200ee0271926c8f2b797218882c0caf0c98
just2so <169085685+just2so@users.noreply.github.com>
2024-10-15 03:23:28 +0800

    Update weather_push.sh

--- weather_push.sh
+++ weather_push.sh
@@ -4,7 +4,7 @@
 OPEN_ID="$OPEN_ID"          # 微信公众号 OpenID
 TEMPLATE_ID="$TEMPLATE_ID"  # 微信模板 ID
 CITY="淄博"                 # 你想查询的城市
-WEATHER_API_URL="https://wttr.in/$CITY?format=%C+%t"
+WEATHER_API_URL="https://wttr.in/$CITY?format=%C+%t&m"
 
 if ! command -v jq &> /dev/null; then
     echo "jq 未安装，请安装 jq 工具。"
